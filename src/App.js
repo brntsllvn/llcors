2cfa686cf8462e28a2108361df03277a66f7229e
Brent Sullivan <brntsllvn@gmail.com>
2018-02-19 21:16:17 -0800

    basic tax calcs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 class TaxForm extends Component {
-  state = {income: 0}
+  state = {income: 0, incomeTax: 0, socialSecurityTax: 0, medicareTax: 0, totalTax: 0}
 
   handleSubmit = event => {
     event.preventDefault()
@@ -10,11 +10,17 @@ class TaxForm extends Component {
 
   updateTax = event => {
     this.setState({
-      income: event.target.value
+      income: event.target.value,
+      incomeTax: Math.round(this.state.income * 0.20 * 100) / 10,
+      socialSecurityTax: Math.round(this.state.income * 0.124 * 100) / 10,
+      medicareTax: Math.round(this.state.income * 0.029 * 100) / 10,
+      totalTax: Math.round(this.state.incomeTax + this.state.socialSecurityTax + this.state.medicareTax)
     })
   }
 
   render() {
+    const {incomeTax, socialSecurityTax, medicareTax, totalTax} = this.state
+
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -26,9 +32,23 @@ class TaxForm extends Component {
         </label>
 
         <br></br>
-
         <label>
-          Income Tax: {this.income * 0.2}
+          Income Tax: {incomeTax}
+        </label>
+
+        <br></br>
+        <label>
+          Social Security Tax: {socialSecurityTax}
+        </label>
+
+        <br></br>
+        <label>
+          Medicare Tax: {medicareTax}
+        </label>
+
+        <br></br>
+        <label>
+          Total Tax: {totalTax}
         </label>
       </form>
     )
