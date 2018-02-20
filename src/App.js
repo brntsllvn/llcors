8fbdfc3be37481efd9f7abdcd68bbda093b94265
Brent Sullivan <brntsllvn@gmail.com>
2018-02-19 17:45:21 -0800

    add simple btn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Button extends Component {
+  render() {
+    return (
+      <button onClick={() => alert('hello')}>Hello</button>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +18,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button />
       </div>
     );
   }
