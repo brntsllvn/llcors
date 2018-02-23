f4e995a62c3494fa959b52e34b2876cfb9a4033e
Brent Sullivan <brntsllvn@gmail.com>
2018-02-22 22:52:12 -0800

    fix state bug

--- src/App.js
+++ src/App.js
@@ -1,55 +1,89 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function TaxItem(props) {
+  return (
+    <div>
+      {props.label} {props.value}
+    </div>
+  )
+}
+
+
 class TaxForm extends Component {
-  state = {income: 0, incomeTax: 0, socialSecurityTax: 0, medicareTax: 0, totalTax: 0}
+  state = {
+    income: 0, 
+    incomeTax: 0, 
+    socialSecurityTax: 0, 
+    medicareTax: 0, 
+    totalTax: 0
+  }
 
   handleSubmit = event => {
     event.preventDefault()
   }
 
+  calculateTax(income, taxRate) {
+    if (!income) {
+      return 0;
+    } else {
+      return income * taxRate;
+    }
+  }
+
   updateTax = event => {
+    console.log(event.target.value)
     this.setState({
-      income: event.target.value,
-      incomeTax: Math.round(this.state.income * 0.20 * 100) / 10,
-      socialSecurityTax: Math.round(this.state.income * 0.124 * 100) / 10,
-      medicareTax: Math.round(this.state.income * 0.029 * 100) / 10,
-      totalTax: Math.round(this.state.incomeTax + this.state.socialSecurityTax + this.state.medicareTax)
+      income: event.target.value
     })
+      // incomeTax: this.calculateTax(this.state.income, 0.2),
+      // socialSecurityTax: Math.round(this.state.income * 0.124 * 100) / 10,
+      // medicareTax: Math.round(this.state.income * 0.029 * 100) / 10,
+      // totalTax: Math.round(this.state.incomeTax + this.state.socialSecurityTax + this.state.medicareTax)
+  }
+
+  renderTaxItem(label, value) {
+    return (
+      <TaxItem 
+        label={label}
+        value={value}
+      />
+    )
   }
 
   render() {
-    const {incomeTax, socialSecurityTax, medicareTax, totalTax} = this.state
+    // const {incomeTax, socialSecurityTax, medicareTax, totalTax} = this.state
 
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <div>
           Salary:
           <input 
             onChange={this.updateTax}
             type="number" 
           />
-        </label>
+        </div>
 
-        <br></br>
-        <label>
-          Income Tax: {incomeTax}
-        </label>
+        <div>
+          {this.renderTaxItem('Income Tax', this.calculateTax(this.state.income, 0.2))}
 
-        <br></br>
-        <label>
+        </div>
+{/*
+        <div>
           Social Security Tax: {socialSecurityTax}
-        </label>
+        </div>
 
-        <br></br>
-        <label>
+        <div>
           Medicare Tax: {medicareTax}
-        </label>
+        </div>
 
-        <br></br>
-        <label>
+        <div>
           Total Tax: {totalTax}
-        </label>
+        </div>
+
+        <div>
+          Total Tax: {totalTax}
+        </div>*/}
       </form>
     )
   }
